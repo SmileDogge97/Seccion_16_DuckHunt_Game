aad24c411cf770916dc0e6de5d79f400d327ea03
EX500791 <ex500791.jose.jarquin@mail.telcel.com>
2024-02-29 10:11:17 -0600

    Ya se cuenta los clicks en el pato

--- app/src/main/java/com/androidavanzado/duckhuntproject/GameActivity.java
+++ app/src/main/java/com/androidavanzado/duckhuntproject/GameActivity.java
@@ -4,21 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import static com.androidavanzado.duckhuntproject.common.Constantes.EXTRA_NICK;
 
 public class GameActivity extends AppCompatActivity {
 
     TextView tvCounterDucks, tvTimer, tvNick;
+    ImageView ivDuck;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        initViewComponent();
+        eventos();
+    }
+
+    private void eventos() {
+        ivDuck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                tvCounterDucks.setText(String.valueOf(counter));
+            }
+        });
+    }
+
+    private void initViewComponent() {
         tvCounterDucks = findViewById(R.id.textViewCounter);
         tvTimer =  findViewById(R.id.textViewTimer);
         tvNick = findViewById(R.id.textViewNick);
+        ivDuck = findViewById(R.id.imageViewDuck);
 
         //cambiar tipo de fuente
         Typeface typeface = Typeface.createFromAsset(getAssets(), "pixel.ttf");
